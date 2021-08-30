6f111a1a015fc55caa39355f93c09cd7b917bb1c
Andy Pearson <andy@londonparkour.com>
2021-08-30 10:11:58 +0100

    update for WP01 and parkourlabs.com

---
+++ wp-content/plugins/real-media-library/vendor/devowl-wp/utils/vendor/composer/installed.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace MatthiasWeb\RealMediaLibrary\Vendor;
+
+return [
+    'root' => [
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'aliases' => [],
+        'reference' => '9eddfed8d2262cbc8f5304e1daaf5fca24803b80',
+        'name' => 'devowl-wp/utils'
+    ],
+    'versions' => [
+        'devowl-wp/utils' => [
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'aliases' => [],
+            'reference' => '9eddfed8d2262cbc8f5304e1daaf5fca24803b80'
+        ]
+    ]
+];
